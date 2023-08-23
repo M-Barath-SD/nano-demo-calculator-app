9cde51a61ea35108b122fce9fc1af1c4d178be39
M Barath <116997312+M-Barath-SD@users.noreply.github.com>
2023-08-23 16:29:17 +0000

    firsttym

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-
 const baseUrl = '/calculator'
-
 app.use(express.json());
-
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
+    return res.send('Hello World!');
     return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
     res.json({ "": null });
 });
 
 
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
-});
-
-app.use(baseUrl, baseRouter);
-app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
-});
+baseRouter.post('/subtract', (req, res)
